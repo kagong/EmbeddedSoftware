167c70f7b56d31a2c9c8957646f6b29fd9cab583
kagong <yun8755975@gmail.com>
2019-05-30 12:43:20 -0700

    proj#3 comment add , Document add

--- proj3/module/stopwatch.c
+++ proj3/module/stopwatch.c
@@ -27,6 +27,7 @@
     TIMER_WRITE;\
 }while(0)
 
+//macros for fnd's data managing 
 #define FND fnd_data[0] << 12 | fnd_data[1] << 8 | fnd_data[2] << 4 | fnd_data[3]
 
 #define TIMER_WRITE outw((unsigned short int)(FND),(unsigned int)iom_fpga_fnd_addr)
@@ -83,6 +84,10 @@ static struct file_operations inter_fops =
     .release = inter_release,
 };
 
+/* name: inter_start
+ * this function register to timer 
+ * for incresing second of watch
+ */
 irqreturn_t inter_start(int irq, void* dev_id, struct pt_regs* reg) {
     printk("start\n");
     if(device_timer.flag == 1)
@@ -92,6 +97,10 @@ irqreturn_t inter_start(int irq, void* dev_id, struct pt_regs* reg) {
     return IRQ_HANDLED;
 }
 
+/* name: inter_pause
+ * this function delete the timer
+ * for stopping watch
+ */
 irqreturn_t inter_pause(int irq, void* dev_id, struct pt_regs* reg) {
     printk("pause\n");
     if(device_timer.flag == 1){
@@ -106,6 +115,10 @@ irqreturn_t inter_pause(int irq, void* dev_id, struct pt_regs* reg) {
     return IRQ_HANDLED;
 }
 
+/* name:inter_reset
+ * this function initiate fnd_data to 0000
+ * and write them
+ */
 irqreturn_t inter_reset(int irq, void* dev_id,struct pt_regs* reg) {
     printk("reset\n");
     device_timer.tic = 0;
@@ -114,19 +127,28 @@ irqreturn_t inter_reset(int irq, void* dev_id,struct pt_regs* reg) {
     return IRQ_HANDLED;
 }
 
+/* name: inter_quit
+ * thie function has two way
+ * one way : try the exit by registering exit timer (3 sec)
+ * two way : delete exit timer Because it is called before 3 sec passed
+ */
 irqreturn_t inter_quit(int irq, void* dev_id, struct pt_regs* reg) {
     printk("exit\n");
-    if(exit_timer.flag == 1){
+    if(exit_timer.flag == 1){//exit fail
         exit_timer.flag = 0;
         del_timer(&exit_timer.timer);
     }
-    else{
+    else{//try exit 
         exit_timer.flag = 1;
-        timer_set(&exit_timer,3*INTVAL,&exit_func);
+        timer_set(&exit_timer,3*INTVAL,&exit_func); //3*Interval == 3sec
     }
     return IRQ_HANDLED;
 }
 
+/* name: timer_func
+ * it called by system timer
+ * this function work incresing timer's sec
+ */
 static void timer_func(unsigned long timeout){
     _device_timer *temp = (_device_timer *)timeout;
     if(++(temp->tic)>=100){
@@ -136,12 +158,20 @@ static void timer_func(unsigned long timeout){
     }
     timer_set(temp,1,&timer_func);
 }
+/* name: exit_func
+ * it called by system timer
+ * this function delete timer, init fnd and
+ * wake up client
+ */
 static void exit_func(unsigned long timeout){
     fnd_data[0] = fnd_data[1] = fnd_data[2] = fnd_data[3] = 0;
     TIMER_WRITE;
     del_timer(&device_timer.timer);
     __wake_up(&que,1,1,NULL);
 }
+/* name: timer_set
+ * this function register timer to system timer
+ */
 static void timer_set(_device_timer *t,int sec,void (*func)(unsigned long)){
     del_timer(&(t->timer));
     t ->timer.expires = jiffies + (sec*HZ/INTVAL);
@@ -154,24 +184,22 @@ static int inter_open(struct inode *minode, struct file *mfile){
     int irq;
 
     printk(KERN_ALERT "Open Module\n");
-    // int1
+    // home
     gpio_direction_input(IMX_GPIO_NR(1,11));
     irq = gpio_to_irq(IMX_GPIO_NR(1,11));
     ret = request_irq(irq,inter_start,IRQF_TRIGGER_RISING,"timer_start",NULL);
 
-    // int2
+    // back
     gpio_direction_input(IMX_GPIO_NR(1,12));
     irq = gpio_to_irq(IMX_GPIO_NR(1,12));
     ret = request_irq(irq,inter_pause,IRQF_TRIGGER_RISING,"timer_pause",NULL);
-    //ret=request_irq();
 
-    // int3
+    // vol+
     gpio_direction_input(IMX_GPIO_NR(2,15));
     irq = gpio_to_irq(IMX_GPIO_NR(2,15));
     ret = request_irq(irq,inter_reset,IRQF_TRIGGER_RISING,"timer_volup",NULL);
-    //ret=request_irq();
 
-    // int4
+    // vol-
     gpio_direction_input(IMX_GPIO_NR(5,14));
     irq = gpio_to_irq(IMX_GPIO_NR(5,14));
     ret = request_irq(irq,inter_quit,IRQF_TRIGGER_FALLING|IRQF_TRIGGER_RISING,"timer_voldown_down",NULL);
