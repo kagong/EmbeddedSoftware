2b8993b49aab7ae519737f7f2c386cd1eeb9baae
kagong <yun8755975@gmail.com>
2019-06-20 22:38:06 -0700

    puzzle add

--- proj4/AndroidEx_2Activity/src/com/example/androidex/MainActivity2.java
+++ proj4/AndroidEx_2Activity/src/com/example/androidex/MainActivity2.java
@@ -19,24 +19,92 @@ public class MainActivity2 extends Activity{
 	int row,col;
 	float widthDp,heightDp;
 	Button btns[];
+    /*
+    private IMyCounterService binder;
+    private ServiceVonnection connection = new ServiceConnection(){
+       // @override
+        public boid onServiceConnection(ComponentName name, IBinder service){
+         //   binder = IMyCounterService.Stub.asInterface(service);
+        //}
+    }*/
 	private void initButtons(){
 		buttonLinear.removeAllViews();
 	}
 	OnClickListener button_listener=new OnClickListener(){
 		public void onClick(View v){
-			if(!((Button)v).getText().toString().equals("0")){
-				v.setBackgroundColor(Color.BLACK);
-
-				((Button)v).setText("0");
-			}
-				
-			else{
-				((Button)v).setText("1");
-				v.setBackgroundResource(android.R.drawable.btn_default);
-			}
-			
+            push_button(Integer.parseInt(v.getText().toString()));
+            if(check_button() == true){
+                initButtons();
+                stopTimer();
+            }
 		}
 	};
+    private void startTimer(){
+        Intent intent = new Intent(MainActivity2.this, MyCounterService.class)
+            bindService(intent,connection,BIND_AUTO_CREATE)
+
+    }
+    private void stopTimer(){
+    }
+    private bool check_button(){
+        for(int i=0;i<row*col-1;i++){
+            if(!btns[i].getText().toString().equals(Integer.toString(i)))
+                return false;
+        }
+        if(btns[row*col-1].getText().toString().equals("0"))
+            return true;
+        return false;
+    }
+    private void swap_button(int idx,int target){
+        btns[target].setBackgroundResource(android.R.drawable.btn_default);
+        btns[target].setText(Integer.toString(idx));
+        btns[idx].setBackgroundColor(Color.BLACK);
+        btns[idx].setText("0");
+    }
+    private void push_button(int idx){
+        if(idx == 0)
+            return ;
+        for(int i=0;i<row*col;i++){
+            if(btns[i].getText().toString().equals(Integer.toString(idx))){
+                int r = i /col,c = i % col;
+                if(r+1 < row){
+                    int target= idx + col;
+                    if(btns[target].getText().toString().equals("0")){
+                        swap_button(idx,target);
+                        break;
+                    }
+        		}
+                if(r-1 >= 0){
+                    int target = idx -col; 
+                    if(btns[target].getText().toString().equals("0")){
+                        swap_button(idx,target);
+                        break;
+                    }
+                }
+                if(c+1 < col){
+                    int target = idx + 1; 
+                    if(btns[target].getText().toString().equals("0")){
+                        swap_button(idx,target);
+                        break;
+                    }
+                }
+                if (c-1 >=0){
+                    int target = idx - 1; 
+                    if(btns[target].getText().toString().equals("0")){
+                        swap_button(idx,target);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    private void makepuzzle(){
+        Random rnd = new Random();
+        for(int i=0;i<20;i++){
+            int idx = rnd.nextInt(row*col-1);
+            push_button(idx);
+        }
+    }
 	private void makeButtons(){
 		btns = new Button[row*col];
 		for(int i= 0 ; i < row; i++){
@@ -54,6 +122,9 @@ public class MainActivity2 extends Activity{
 			}
 			buttonLinear.addView(test);
 		}
+        btns[row*col -1].setText("0");
+        btns[row*col -1].setBackgroundColor(Color.BLACK);
+        make_puzzle();
 	}
 	
 		@Override
@@ -81,6 +152,7 @@ public class MainActivity2 extends Activity{
 				col = Integer.parseInt(datas[1]);
 				initButtons();
 				makeButtons();
+                startTimer():
 			}
 		};
 		makeBtn.setOnClickListener(make_listener);
